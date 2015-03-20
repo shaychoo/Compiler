125c219ea126a13427665786028b8781c3d806df
Shay Trjmn <shaytrjmn@gmail.com>
2015-03-20 13:24:12 +0200

    ADD main

--- src/IC/Main.java
+++ src/IC/Main.java
@@ -2,9 +2,12 @@ package IC;
 
     //import IC.Parser.*;
 
+    import IC.AST.PrettyPrinter;
+    import IC.AST.StatementsBlock;
     import IC.Parser.*;
     import IC.Parser.Token;
     import java_cup.anttask.CUPTask;
+    import java_cup.runtime.Symbol;
     import jflex.anttask.JFlexTask;
 
     import java.io.File;
@@ -13,8 +16,6 @@ package IC;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
 //        CUPTask cupTask = new CUPTask();
 //        JFlexTask jFlex = new JFlexTask();
 //
@@ -32,9 +33,58 @@ public class Main
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
+        private static boolean printtokens = false;
+
+        /** Reads an SLP and pretty-prints it.
+         *
+         * @param args Should be the name of the file containing an SLP.
+         */
+        public static void main(String[] args) {
+        try {
+            if (args.length == 0) {
+                System.out.println("Error: Missing input file argument!");
+                printUsage();
+                System.exit(-1);
+            }
+            if (args.length == 2) {
+                if (args[1].equals("-printtokens")) {
+                    printtokens = true;
+                }
+                else {
+                    printUsage();
+                    System.exit(-1);
+                }
+            }
+
+            // Parse the input file
+            FileReader txtFile = new FileReader(args[0]);
+            Lexer scanner = new Lexer(txtFile);
+            Parser parser = new Parser(scanner);
+            parser.printTokens = printtokens;
+
+            Symbol parseSymbol = parser.parse();
+            System.out.println("Parsed " + args[0] + " successfully!");
+            StatementsBlock root = (StatementsBlock) parseSymbol.value;
+
+            // Pretty-print the program to System.out
+           // PrettyPrinter printer = new PrettyPrinter(root);
+            //printer.print();
+
+            // Interpret the program
+            //SLPEvaluator evaluator = new SLPEvaluator(root);
+            //evaluator.evaluate();
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+    }
+
+        /** Prints usage information about this application to System.out
+         */
+        public static void printUsage() {
+        System.out.println("Usage: slp file [-printtokens]");
+    }
 
 
 
     }
-}
 
